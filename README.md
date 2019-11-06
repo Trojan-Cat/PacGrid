df0646e7f3e160c5df4f1d824e1a602805920eb5
Trojan-Cat <kajarrod@gmail.com>
2019-11-06 21:31:20 +1100

    Readline works to input commands

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
-This is a pacman simulator using JS
+This is a pacman simulator using JS, it is to be run in the terminal and uses the readline module in order to do this (A poor mans Colossal Cave Adventure)
 
-* Clone the git
-* Navigate to the file and open a terminal
-* npm start to run the application
+# Instalation
+
+NODE.js needs to be installed
+
+- Clone the git
+- Navigate to the file and open a terminal
+- npm install
+- npm start to run the application
+
+# Commands for the game
+
+Welcome to Pacman, you will control a pacman placed onto a 5x5 grid
+PLACE, will take in an X, Y, and direction cord
+e.g: PLACE 1,2,NORTH
+MOVE will move the pacman forwards by one step
+LEFT will turn the packman to their left
+RIGHT will turn the packman to their right
+REPORT will tell you where the pacman is
+H will bring up the controls
+Q will exit the program
--- index.js
+++ index.js
@@ -1,60 +1,72 @@
+// Needed a way to make this terminal based
 const readline = require("readline");
+//Using associative arry for the faces, so to easily change and check the input of a face to the array
+let faces = { NORTH: 0, EAST: 1, SOUTH: 2, WEST: 3 };
 
+// The readline is fairly boilerplate from nodejs.org
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question("Please input a command for pacman? (H for controls)", answer => {
-  // TODO: Log the answer in a database
-  switch (answer) {
-    case "PLACE":
-      console.log("wias");
-      break;
-    case "MOVE":
-      MOVE();
-      break;
-    case "Q":
-      rl.close();
-      break;
-    case "H":
-      Welcome();
-      break;
+// This function will call itself again to get the new input, unless Q is entered which closes the readline
+var recursiveAsyncReadLine = function() {
+  rl.question(
+    "Please input a command for pacman? (H for controls):",
+    answer => {
+      let testStr = answer.substring(0, 5);
+      if (testStr === "PLACE") {
+        breakPlace(answer);
+        //PLACE();
+        recursiveAsyncReadLine();
+      } else {
+        switch (answer) {
+          case "MOVE":
+            MOVE();
+            recursiveAsyncReadLine();
+            break;
+          case "LEFT":
+            LEFT();
+            recursiveAsyncReadLine();
+            break;
+          case "RIGHT":
+            RIGHT();
+            recursiveAsyncReadLine();
+            break;
+          case "REPORT":
+            REPORT();
+            recursiveAsyncReadLine();
+            break;
+          case "Q":
+            rl.close();
+            break;
+          case "H":
+            Welcome();
+            recursiveAsyncReadLine();
+            break;
+          default:
+            console.log("Sorry that is not a command");
+            recursiveAsyncReadLine();
+            break;
+        }
+      }
+    }
+  );
+};
 
-    default:
-      break;
-  }
+// Starting the read line
+recursiveAsyncReadLine();
 
-  rl.on("resume", () => {
-    console.log("Readline resumed.");
-  });
-});
-
-// Creating a basic map that is five by five with just manually entering the numbers at first
-// TODO: Change so that Map Height and width can be parameters to enter at start of new game
-let maps = [
-  [0, 0, 0, 0],
-  [1, 1, 1, 1],
-  [2, 2, 2, 2],
-  [3, 3, 3, 3],
-  [4, 4, 4, 4]
-];
-const face = [0, 1, 2, 3];
-// Rather than north, east, etc.. I am using 0, 1, 2, 3 that way if it is left,
-// I can just minus 1 and if right I just add 1, and when it is > 3 it resets to 0, and < 0 changes to 3
-
-//  Default start position
+//  Default start position will be out of the map therefore the Pacman won't move till a valid pacman is initialized
 //let Pacman = { x: -1, y: -1, f: -1 };
 let Pacman = { x: 0, y: 0, f: 0 };
 
-exports.test = function() {
-  console.log("YIIP");
-};
-
+// Welcome shows the user how to play the game
 const Welcome = () =>
   console.log(`Welcome to Pacman, you will control a pacman placed onto a 5x5 grid
 Commands:
-Place, will take in an X, Y, and direction cord
+PLACE, will take in an X, Y, and direction cord
+e.g: PLACE 1,2,NORTH
 MOVE will move the pacman forwards by one step
 LEFT will turn the packman to their left
 RIGHT will turn the packman to their right
@@ -62,7 +74,28 @@ REPORT will tell you where the pacman is
 H will bring up the controls
 Q will exit the program`);
 
+// If placements are to be made as a singlestring this will check through and break it up
+// This does not do any checking but just returning where a value should be in the string
+let breakPlace = ans => {
+  let placement = ans.trim().substring(5, 15);
+  let x = placement.substring(1, 2);
+  let y = placement.substring(3, 4);
+  let f = placement.substring(5, 10);
+  PLACE(x, y, f);
+};
+
+// This parses the string that was entered, it checks to see for a match in the associate array and then if it's there get's the value returned
+let convertFace = f => {
+  if (faces[f] === undefined) {
+    return -1;
+  } else {
+    return faces[f];
+  }
+};
+
+// Some of this checking is not that relevant anymore
 function PLACE(x, y, f) {
+  f = convertFace(f);
   if (x > 4 || x < 0) {
     Pacman = { x: -1, y: -1, f: -1 };
     console.log("x Value has to be between 0 and 4");
@@ -81,8 +114,10 @@ function PLACE(x, y, f) {
 }
 
 // Left and right commands just turns where pacman is facing
+// The first if statement checks if the pacman last placed in valid, if not it skips the command
+// TODO: Change the first if to be a check function?
 function LEFT() {
-  if (Pacman.x != -1) {
+  if (Pacman.f != -1) {
     if (Pacman.f === 0) {
       Pacman.f = 3;
     } else {
@@ -93,7 +128,7 @@ function LEFT() {
 }
 
 function RIGHT() {
-  if (Pacman.x != -1) {
+  if (Pacman.f != -1) {
     if (Pacman.f === 3) {
       Pacman.f = 0;
     } else {
@@ -101,8 +136,9 @@ function RIGHT() {
     }
   }
 }
+
 function MOVE() {
-  if (Pacman.x != -1) {
+  if (Pacman.f != -1) {
     let oldMan = Pacman;
     if (Pacman.f === 0 && Pacman.y != 4) {
       Pacman = { x: oldMan.x, y: oldMan.y + 1, f: 0 };
